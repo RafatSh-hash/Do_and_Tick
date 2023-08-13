8f5149331b78bb2bcc600b4f5ea71467a2e97786
Sadman Yasir Rafat <rafatsh800@gmail.com>
2023-08-13 14:19:42 +0600

    Fixed style and function

--- script.js
+++ script.js
@@ -8,27 +8,95 @@ const formattedDate = currentDate.toLocaleDateString(undefined, options);
 currentDateElement.textContent = formattedDate;
 
 
-function handleSubmit(event) {
-    event.preventDefault();
-    const task = document.getElementById("task").value;
-    console.log(task);
-    const toDo = document.getElementById("todo");
-    // creating a div for each task
+// function handleSubmit(event) {
+//     event.preventDefault();
+//     const task = document.getElementById("task").value;
+//     console.log(task);
+//     const toDo = document.getElementById("todo");
+//     // creating a div for each task
+//     const newTaskDiv = document.createElement("div");
+//     //Styling the div
+//     newTaskDiv.style.display = "flex";
+//     newTaskDiv.style.justifyContent = "center";
+//     newTaskDiv.style.alignItems = "center";
+//     newTaskDiv.style.paddingTop = "10px";
+//     newTaskDiv.style.paddingBottom = "10px";
+//     //creating a checkbox for each task
+//     var checkbox = document.createElement('input');
+//     checkbox.type = 'checkbox';
+//     checkbox.name = 'selectCheckbox';
+//     checkbox.id = 'checkbox';
+    
+
+//     //creating a p for each task
+//     const paragraph = document.createElement('p');
+//     paragraph.style.marginRight = '10px';
+//     paragraph.style.marginLeft = '10px';
+//     paragraph.style.paddingTop = "10px";
+//     paragraph.style.paddingBottom = "10px";
+//     paragraph.innerText = task;
+//     newTaskDiv.appendChild(checkbox);
+//     newTaskDiv.appendChild(paragraph);
+//     toDo.appendChild(newTaskDiv);
+
+//     checkbox.addEventListener('change', function() {
+//         if (checkbox.checked) {
+//             // Checkbox is checked
+//             console.log('Checkbox checked for:', task);
+//             const complete = document.getElementById('complete');
+//             paragraph.style.textDecoration = "line-through";
+//             complete.appendChild(paragraph)
+//             toDo.removeChild(newTaskDiv);
+//             if (toDo.innerHTML.trim() === '') {
+//                 window.alert('Congratulations! You have successfully completed');
+//             } else {
+//                 console.log('myDiv is not empty');
+//             }
+            
+
+//             // You can add further actions here when the checkbox is checked
+//         } else {
+//             // Checkbox is unchecked
+//             console.log('Checkbox unchecked for:', task);
+//             // You can add further actions here when the checkbox is unchecked
+//         }
+//     });
+    
+
+        
+
+
+//     form.reset()
+  
+
+// }
+const form = document.getElementById("form");
+const toDo = document.getElementById("todo");
+const complete = document.getElementById("complete");
+
+// Load tasks from localStorage
+const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+function displayTasks() {
+    savedTasks.forEach(function(task) {
+        createTaskElement(task);
+    });
+}
+
+function createTaskElement(task) {
     const newTaskDiv = document.createElement("div");
-    //Styling the div
+    // Styling the div
     newTaskDiv.style.display = "flex";
     newTaskDiv.style.justifyContent = "center";
     newTaskDiv.style.alignItems = "center";
     newTaskDiv.style.paddingTop = "10px";
     newTaskDiv.style.paddingBottom = "10px";
-    //creating a checkbox for each task
-    var checkbox = document.createElement('input');
+
+    const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.name = 'selectCheckbox';
     checkbox.id = 'checkbox';
-    
 
-    //creating a p for each task
     const paragraph = document.createElement('p');
     paragraph.style.marginRight = '10px';
     paragraph.style.marginLeft = '10px';
@@ -43,30 +111,35 @@ function handleSubmit(event) {
         if (checkbox.checked) {
             // Checkbox is checked
             console.log('Checkbox checked for:', task);
-            const complete = document.getElementById('complete');
             paragraph.style.textDecoration = "line-through";
-            complete.appendChild(paragraph)
+            complete.appendChild(paragraph);
             toDo.removeChild(newTaskDiv);
-            if (toDo.innerHTML.trim() === '') {
-                window.alert('Congratulations! You have successfully completed');
-            } else {
-                console.log('myDiv is not empty');
+
+            // Remove task from savedTasks array and localStorage
+            const index = savedTasks.indexOf(task);
+            if (index > -1) {
+                savedTasks.splice(index, 1);
+                localStorage.setItem('tasks', JSON.stringify(savedTasks));
             }
-            
 
-            // You can add further actions here when the checkbox is checked
-        } else {
-            // Checkbox is unchecked
-            console.log('Checkbox unchecked for:', task);
-            // You can add further actions here when the checkbox is unchecked
-        }
+            if (toDo.innerHTML.trim() === '') {
+                window.alert('Congratulations! You have successfully completed all tasks.');
+                const noTask = document.createElement('div');
+                noTask.innerHTML = `<h1 class="text-3xl">You have no task left</h1>`;
+                toDo.appendChild(noTask);
+            }
+        } 
     });
-    
-
-        
-
-
-    form.reset()
-  
-
 }
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const task = document.getElementById("task").value;
+    savedTasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(savedTasks));
+    createTaskElement(task);
+    form.reset();
+}
+
+// Display tasks when the page loads
+displayTasks();
